0f2faf8d1c013ee80cc45fa24da7ecc671ca7e66
Jake Blackburn <jakeblackburn@Johns-MacBook-Air.local>
2022-09-20 20:22:20 -0400

    added colorizer modes

--- colorizer/colorizer.py
+++ colorizer/colorizer.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-def colorize(path, target, tolerance, new_color):
+def colorize_by_tolerance(path, target, tolerance, new_color):
     image = Image.open(path).convert('RGB')
 
     pixels = image.getdata()
@@ -27,6 +27,45 @@ def colorize(path, target, tolerance, new_color):
             g > (target[1] - tolerance[1]) and 
             b < (target[2] + tolerance[2]) and 
             b > (target[2] - tolerance[2])
+        ):
+            new_pixels[pointer] = new_color
+
+        pointer += 1
+
+    newImage = Image.new("RGB", image.size)
+    newImage.putdata(new_pixels)
+
+    return newImage
+
+
+
+def colorize_by_range(path, high_target, low_target, new_color):
+    image = Image.open(path).convert('RGB')
+
+    pixels = image.getdata()
+
+    new_pixels = []
+
+    for p in pixels:
+        new_pixels.append(p)
+
+    pointer = 0
+
+    while pointer < len(new_pixels):
+
+        p = new_pixels[pointer]
+
+        r = p[0]
+        g = p[1]
+        b = p[2]
+
+        if (
+            r < (high_target[0]) and 
+            r > (low_target[0]) and 
+            g < (high_target[1]) and 
+            g > (low_target[1]) and 
+            b < (high_target[2]) and 
+            b > (low_target[2])
         ):
             new_pixels[pointer] = new_color
 
--- colorizer/main.py
+++ colorizer/main.py
@@ -1,9 +1,21 @@
-from colorizer import colorize
+from colorizer import colorize_by_tolerance, colorize_by_range
 
+mode = 'tolerance'
 path = 'image.png'
 target = [150, 10, 10]
 tolerance = [30, 10, 10]
 new_color = (100, 200, 245)
 
-image = colorize(path, target, tolerance, new_color)
-image.save('colorized.png', optimize=True)
+# mode = 'range'
+# path = 'image.png'
+# high_target = [175, 20, 20]
+# low_target = [140, 0, 0]
+# new_color = (100, 200, 245)
+
+if (mode == 'tolerance'):
+    image = colorize_by_tolerance(path, target, tolerance, new_color)
+    image.save('colorized.png', optimize=True)
+
+if (mode == 'range'):
+    image = colorize_by_range(path, high_target, low_target, new_color)
+    image.save('colorized.png', optimize=True)
